83c983aa659e47a455134530fc45f3b8d7ebbd80
Lucas Rodriguez <110471982+lucasrdrz@users.noreply.github.com>
2026-04-27 17:32:15 -0300

    Add files via upload

---
+++ Prode.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import streamlit as st
+
+st.title("🏆 Prode Mundial")
+
+# URLs (CAMBIAR GID)
+url_partidos = "https://docs.google.com/spreadsheets/d/1q_1I2ShBwUFwqJ1RJ0SVjr-VnYbeQS8TiqB0hrSB5TM/gviz/tq?tqx=out:csv&gid=0"
+url_predicciones = "https://docs.google.com/spreadsheets/d/1q_1I2ShBwUFwqJ1RJ0SVjr-VnYbeQS8TiqB0hrSB5TM/gviz/tq?tqx=out:csv&gid=299285810"
+
+# Cargar datos
+partidos = pd.read_csv(url_partidos)
+predicciones = pd.read_csv(url_predicciones)
+
+#st.write("Columnas PARTIDOS:", partidos.columns)
+#st.write("Columnas PREDICCIONES:", predicciones.columns)
+# Mostrar partidos
+st.subheader("📅 Partidos")
+st.dataframe(partidos)
+
+# Filtrar solo partidos con resultado cargado
+partidos_jugados = partidos.dropna(subset=["Resultado"])
+
+# Merge
+df = predicciones.merge(
+    partidos_jugados,
+    left_on="Partido_ID",
+    right_on="ID"
+)
+
+# Calcular puntos
+df["Puntos"] = (df["Prediccion"] == df["Resultado"]).astype(int)
+
+# Ranking
+tabla = df.groupby("Usuario")["Puntos"].sum().reset_index()
+tabla = tabla.sort_values(by="Puntos", ascending=False)
+
+st.subheader("🏆 Tabla de posiciones")
+st.dataframe(tabla)
